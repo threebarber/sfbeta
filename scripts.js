a52dcfc7e3aeeb870e512b4590f4df07d9c80e3e
threebarber <dvtdropshot@gmail.com>
2022-08-06 14:34:50 -0400

    basic framework, loading of articles into divs dynamically

--- scripts.js
+++ scripts.js
@@ -1,14 +1,47 @@
+        
+        async function getData(url) {
+            const response = await fetch(url);
+
+            return response.json();
+        }
 
 
-function retrieveArticleData(){
-    const getJSON = async url => {
-        const response = await fetch(url);
-        return response.json(); // get JSON from the response 
-      }
-      
-      console.log("Fetching data...");
-      getJSON("https://api.spaceflightnewsapi.net/v3/articles")
-        .then(data => console.log(data));
-}
+        function loopData(){
+            
+            (async () => {
+                const data = await getData("https://api.spaceflightnewsapi.net/v3/articles")
+              
+                console.log(data);
+
+                data.forEach(article => {
+    
+                    console.log(article["id"]);
+                    console.log(article["title"]);
+
+                    var articleDiv = document.createElement("div");
+                    articleDiv.classList.add("articleDiv");
+
+                    var articleTitle = document.createElement("h3");
+                    articleTitle.innerText = article["title"];
+                    articleDiv.appendChild(articleTitle);    
 
 
+                    var articleSec = document.querySelector(".mainDiv")
+                    
+                    articleSec.appendChild(articleDiv);
+                });
+            
+            
+            })()
+
+
+            
+        }
+
+
+        /*for (let key in article) {
+    console.log(`${article}: ${article[key]}`);
+}*/
+
+
+loopData();
