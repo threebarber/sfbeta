bcd8908da632ad17a2c7e6f392893a0e179e9acd
threebarber <dvtdropshot@gmail.com>
2022-08-07 18:00:19 -0400

    added buttons, change of title, added blog functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+document.getElementById("newsButton").addEventListener("click", function(){
+    loadArticles();
+ });
+
+ document.getElementById("blogButton").addEventListener("click", function(){
+    loadBlogs();
+ });
+
+
 
 async function getData(url) {
     const response = await fetch(url);
@@ -7,6 +16,8 @@ async function getData(url) {
 
 
 function loadArticles() {
+
+    document.querySelector(".mainDiv").innerHTML = "";
 
     (async () => {
         const data = await getData("https://api.spaceflightnewsapi.net/v3/articles")
@@ -55,10 +66,63 @@ function loadArticles() {
 
 
     })()
-
-
-
 }
+
+
+
+function loadBlogs() {
+
+    document.querySelector(".mainDiv").innerHTML = "";
+
+    (async () => {
+        const data = await getData("https://api.spaceflightnewsapi.net/v3/blogs")
+
+        console.log(data);
+
+        data.forEach(article => {
+
+            console.log(article["id"]);
+            console.log(article["title"]);
+
+            var articleDiv = document.createElement("div");
+            articleDiv.classList.add("articleDiv");
+
+            articleDiv.onclick = function () {
+                window.open(article["url"], '_blank').focus();
+
+            }
+
+
+            var articleImage = document.createElement("img");
+            articleImage.src = article["imageUrl"];
+            articleImage.classList.add("articleImg");
+            articleDiv.appendChild(articleImage);
+
+
+            var divider = document.createElement("hr");
+            articleDiv.appendChild(divider);
+
+
+            var articleTitle = document.createElement("h3");
+            articleTitle.innerText = article["title"];
+            articleDiv.appendChild(articleTitle);
+
+
+            var articleSum = document.createElement("p");
+            var shortSummary = shortenSummary(article["summary"]);
+            articleSum.innerText = shortSummary;
+            articleDiv.appendChild(articleSum);
+
+
+            var articleSec = document.querySelector(".mainDiv")
+
+            articleSec.appendChild(articleDiv);
+        });
+
+
+    })()
+}
+
 
 /* shorten summary text so that it will fit better as some are very long*/
 
@@ -76,5 +140,3 @@ function shortenSummary(summaryText) {
 console.log(`${article}: ${article[key]}`);
 }*/
 
-
-loadArticles();
