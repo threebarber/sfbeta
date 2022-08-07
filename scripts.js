06ca00ed9a27c25671b4bb6fe8764139e36d1b0f
threebarber <dvtdropshot@gmail.com>
2022-08-07 10:43:19 -0400

    summary fixes so they aren't so long and everything fits better

--- scripts.js
+++ scripts.js
@@ -1,67 +1,79 @@
-        
-        async function getData(url) {
-            const response = await fetch(url);
 
-            return response.json();
-        }
+async function getData(url) {
+    const response = await fetch(url);
 
-
-        function loadArticles(){
-            
-            (async () => {
-                const data = await getData("https://api.spaceflightnewsapi.net/v3/articles")
-              
-                console.log(data);
-
-                data.forEach(article => {
-    
-                    console.log(article["id"]);
-                    console.log(article["title"]);
-
-                    var articleDiv = document.createElement("div");
-                    articleDiv.classList.add("articleDiv");
-
-                    articleDiv.onclick = function(){
-                        window.open(article["url"], '_blank').focus();
-
-                    }
+    return response.json();
+}
 
 
-                    var articleImage = document.createElement("img");
-                    articleImage.src = article["imageUrl"];
-                    articleImage.classList.add("articleImg");
-                    articleDiv.appendChild(articleImage);
+function loadArticles() {
+
+    (async () => {
+        const data = await getData("https://api.spaceflightnewsapi.net/v3/articles")
+
+        console.log(data);
+
+        data.forEach(article => {
+
+            console.log(article["id"]);
+            console.log(article["title"]);
+
+            var articleDiv = document.createElement("div");
+            articleDiv.classList.add("articleDiv");
+
+            articleDiv.onclick = function () {
+                window.open(article["url"], '_blank').focus();
+
+            }
 
 
-                    var divider = document.createElement("hr");
-                    articleDiv.appendChild(divider);
+            var articleImage = document.createElement("img");
+            articleImage.src = article["imageUrl"];
+            articleImage.classList.add("articleImg");
+            articleDiv.appendChild(articleImage);
 
 
-                    var articleTitle = document.createElement("h3");
-                    articleTitle.innerText = article["title"];
-                    articleDiv.appendChild(articleTitle);   
-                    
-                    
-                    var articleSum = document.createElement("p");
-                    articleSum.innerText = article["summary"];
-                    articleDiv.appendChild(articleSum);
+            var divider = document.createElement("hr");
+            articleDiv.appendChild(divider);
 
 
-                    var articleSec = document.querySelector(".mainDiv")
-                    
-                    articleSec.appendChild(articleDiv);
-                });
-            
-            
-            })()
+            var articleTitle = document.createElement("h3");
+            articleTitle.innerText = article["title"];
+            articleDiv.appendChild(articleTitle);
 
 
-            
-        }
+            var articleSum = document.createElement("p");
+            var shortSummary = shortenSummary(article["summary"]);
+            articleSum.innerText = shortSummary;
+            articleDiv.appendChild(articleSum);
 
 
-        /*for (let key in article) {
-    console.log(`${article}: ${article[key]}`);
+            var articleSec = document.querySelector(".mainDiv")
+
+            articleSec.appendChild(articleDiv);
+        });
+
+
+    })()
+
+
+
+}
+
+/* shorten summary text so that it will fit better as some are very long*/
+
+function shortenSummary(summaryText) {
+
+    const wordCount = summaryText.split(' ').length;
+
+    let shortSummary = summaryText.split(' ').slice(0, wordCount * .75).join(' ') + "...";
+
+    return shortSummary;
+
+}
+
+/*for (let key in article) {
+console.log(`${article}: ${article[key]}`);
 }*/
 
 
